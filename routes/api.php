cb8f86626946c5381398cf73f69fb7c637eec61d
Lee Muddy <lensig13@gmail.com>
2023-09-06 14:14:45 +0200

    add-api-routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthAPIController;
+use App\Http\Controllers\API\CardAPIController;
+use App\Http\Controllers\API\MerchantAPIController;
+use App\Http\Controllers\API\RegistrationAPIController;
+use App\Http\Controllers\API\CardSwitchTaskAPIController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +18,68 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('users')->as('users.')->group(function () {
+
+    Route::controller(RegistrationAPIController::class)->group(
+        static function (): void {
+
+            Route::post('register', 'register')->name('register');
+
+        }
+    );
+
+    Route::prefix('auth')->as('auth.')->controller(AuthAPIController::class)->group(
+        static function (): void {
+
+            Route::post('login', 'login')->name('login');
+
+            Route::middleware('auth:sanctum')->group(static function (): void {
+                Route::get('logout', 'logout')->name('logout');
+
+                Route::get('refresh', 'refresh')->name('refresh');
+
+                Route::get('user', 'user')->name('user');
+            });
+
+        }
+    );
+
 });
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::prefix('cards')
+        ->controller(CardAPIController::class)
+        ->group(function () {
+
+        Route::get('', 'index')->name('index');
+
+        Route::post('', 'store')->name('store');
+
+    });
+
+
+    Route::prefix('merchants')
+        ->controller(MerchantAPIController::class)
+        ->group(function () {
+
+        Route::get('', 'index')->name('index');
+
+    });
+
+    Route::prefix('card-switch-tasks')
+        ->as('card-switch-tasks')
+        ->controller(CardSwitchTaskAPIController::class)
+        ->group(function () {
+
+        Route::get('', 'index')->name('index');
+
+        Route::post('', 'store')->name('store');
+
+        Route::match(['patch', 'put'], '{id}/markTaskFinished', 'markTaskFinished')->name('update.markTaskFinished');
+
+        Route::match(['patch', 'put'], '{id}/markTaskFailed', 'markTaskFailed')->name('update.markTaskFailed');
+
+    });
+});
+
